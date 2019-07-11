5c98dd64028021434a48f30aa735eb951927c966
Youngju Lee <lucylee839@hotmail.com>
2019-07-11 10:50:33 -0700

    index and app js and css organized

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from "react";
 import Game from "./components/Game/Game";
-// import NavBar from "./page/navbar.js";
-import './App.css';
 
 function App() {
   return(
-
-
     <Game />
-  
   );
 }
 
